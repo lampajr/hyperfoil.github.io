603d9bb61bee1d5f8c22c9b34bb1f53e6320f9d8
Radim Vansa <rvansa@redhat.com>
2019-10-08 10:12:00 +0200

    Fix multiple code samples on the same page

---
+++ assets/scripts/codesample.js
@@ -0,0 +1,34 @@
+function insertCodesample(where, tag, src, slice) {
+    var pre = document.createElement('pre');
+    pre.className = "highlight";
+    var code = document.createElement('code');
+    code.className = "prettyprint";
+    code.id = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    pre.insertBefore(code, null);
+    where.parentNode.insertBefore(pre, document.currentScript)
+    fetch('https://raw.githubusercontent.com/Hyperfoil/Hyperfoil/' + tag + '/' + src , { cache: "reload"})
+      .then(function(response) {
+          if (!response.ok) {
+              fetch('https://raw.githubusercontent.com/Hyperfoil/Hyperfoil/master/' + src, { cache: "reload" })
+                .then(function(response) {
+                    response.text().then(function (text) {
+                    if (slice != null) {
+                      slice = slice.split(":");
+                      text = text.split(/\r?\n/).slice(slice[0], slice[1]).join("\n");
+                    }
+                      code.innerHTML = text;
+                      prettyPrint();
+                    })
+                })
+          } else {
+            response.text().then(function (text) {
+              if (slice != null) {
+                slice = slice.split(":");
+                text = text.split(/\r?\n/).slice(slice[0], slice[1]).join("\n");
+              }
+              code.innerHTML = text;
+              prettyPrint();
+            })
+          }
+      })
+  }
